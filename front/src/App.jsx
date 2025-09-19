700367689b69ec4c175c3c5085a1bd1dd25c3d25
DarkWoodsHunter <aserrods@gmail.com>
2025-09-18 21:35:39 -0300

    Upload to github

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h3 className=' text-red-500'>Hello</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
