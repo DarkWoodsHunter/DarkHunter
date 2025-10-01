bd84b8ccda16cf8a85b318ff0d17b348ee31cc01
DarkWoodsHunter <aserrods@gmail.com>
2025-09-30 22:43:03 -0300

    update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <div className=' w-[80%] bg-sky-800 mx-auto rounded-4xl my-5'>
         <h3 className=' text-red-500'>Hello</h3>
         
+        
       </div>
     </>
   )
